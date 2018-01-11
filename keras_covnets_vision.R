54cdd155951463783b717faad64d7affe4505012
michelleclements <michelleclements@gmail.com>
2018-01-11 10:58:57 +0000

    updating covnets

--- keras_covnets_vision.R
+++ keras_covnets_vision.R
@@ -60,6 +60,7 @@ results
 #' * The kernel_size tells the layer what size to look at. above is 3x3 squares.  
 #' * Patterns can then be recognised anywhere in the image and not just e.g. in the bottom corner. They are said to be translation invariant.  
 #' * Covnets can learn spatial hierarchies - each successive layer zooms in on the image a bit more.  
+#' * Covnets require less image pre-processing than other options.  
 #' 
 #' 
 #' ### Covnet details: 
@@ -74,12 +75,55 @@ results
 #' * 26 is the number of 3 pixel groups along a single length 28 row (from kernel_size).  
 #' * 32 is the number of filters specified.  
 #' * Each filter is a 26x26 grid of values. This is the response map of the filter over the input.  
-#' * Feature map: the colleciton of 2D spatial maps produced by each filter over the input.  
+#' * Feature map: the collection of 2D spatial maps produced by each filter over the input.  
+#' * The covnet slides over all squares on the input map.  
+#' * Within each square, the window height, window depth and input depth are extracted.  
+#' * The square is then transformed into a 1D vector of length output_depth.  
+#' * The transformation is by the convolutional kernel: a tensor product with teh same learned weight matrix.  
+#' * The output vectors from all of the squares are then assembled into a 3D output feature map.  
+#' * Note that information from one pixel is now in multiple adjacent points of the feature map.  
+#' * You can use padding to get the output feature map the same height and width as the input. Use padding = "same".  
+#' * Multiple covnets keep bringing in local information from a little bit further out? 
+#'  
 #' 
 #' 
 #' The main parameters varied in covnets are:
 #' * The size of the patches looked at (kernel_size). Typically 3x3 or 5x5.   
-#' * The number of filters. Also known as the depth of the output feature map. 
+#' * The number of filters. Also known as the depth of the output feature map. Tends to be more in deeper layers.   
+#' * Also, padding = same to get output height and width the same as the input.  
+#' * Note that as the network gets deeper, the height and width gets less but the number of filters increases.  
 #' 
 #' 
+#' ### Max pooling layer  
+#' * Max pooling reduces the height and width.  
+#' * It takes its input feature map and creates windows across the feature space.  
+#' * e.g. a 2x2 pool size above will lay 2x2 grids across the whole 26x26 space.  
+#' * It will then take the maximum value of all 4 blocks that are in each grid.  
+#' * e.g. above will output a 13x13 grid. 
+#' * cov layers use stride = 1, so that there's overlap. Max pooling uses 2x2 and stride = 2 so there's no overlap.  
+#' * Average pooling can also be used, but max tends to be more effective.  
+#' 
+#' Max pooling has two effects: 
+#' 1. It reduces the number of co-effecients to process and helps to reduce overfitting.   
+#' 2. It makes each node look at a relatively bigger space which induces spatial hierarchies. 
+#' 
+#' ### End the cov net with a classifier   
+#' * use layer_flatten to bring dimensions down.  
+#' * then optionally add a layer_dropout
+#' * then layer_dese with activation relu and at least as many units as filters in the cov2d layer  
+#' * then layer_dense with the required output activation function.  
+#' 
+#' ### Improving the power of covnets
+#' * covnets can be trained with relatively small amounts of data if the problem is simple.  
+#' 
+#' Data augmentation can help mitigate overfitting:
+#' * Augment the data by adding in samples that are randomly changed from original samples (e.g. squishing, rotating).  
+#' * Samples are then not independent, but additional info might help the model. 
+#' * Might help to add a layer_dropout after layer_flatten to avoid overfitting.  
+#'  
+#' Using pre-trained networks (e.g. ImageNet) can also help to improve models.  
+#' * feature extraction with a pre-trained network - add a new classifier onto an existing trained convolutional base.  
+#' * fine-tuning a pre-trained network - fine tune the final covnet after doing feature extraction above.       
 
+
+#' end of chapter 5 has some stuff on visualising covnets.  
